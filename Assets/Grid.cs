07767a9b8f6f680a6f09c572a68406c295d68261
Kuoyuan Yang <1kuoyuanyang@gmail.com>
2017-04-15 16:24:59 -0400

    scripts

---
+++ Assets/Grid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+	
+	public static int w = 10;
+	public static int h = 20;
+	public static Transform[,] grid = new Transform[w, h];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public static Vector2 roundVec2(Vector2 v) {//returns a new vector with whole number position
+		return new Vector2(Mathf.Round(v.x),
+			Mathf.Round(v.y)); 
+	}
+	public static bool insideBorder(Vector2 pos) {//checks if a block is within border
+		return ((int)pos.x >= 0 &&
+			(int)pos.x < w &&
+			(int)pos.y >= 0);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/spawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour {
+	
+	public GameObject[] boxList;
+
+	// Use this for initialization
+	void Start () {
+		SpawnNewBox();
+	}
+
+	public void SpawnNewBox() {
+		int i = Random.Range(0, boxList.Length);
+		Instantiate(boxList[i], transform.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
