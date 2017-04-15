3f26be14fa344fa6d20a64b8031d94f16ae40887
Kuoyuan Yang <1kuoyuanyang@gmail.com>
2017-04-15 17:34:10 -0400

    all scripts

--- Assets/spawn.cs
+++ Assets/spawn.cs
@@ -8,10 +8,10 @@ public class spawn : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		SpawnNewBox();
+		SpawnNew();
 	}
 
-	public void SpawnNewBox() {
+	public void SpawnNew() {
 		int i = Random.Range(0, boxList.Length);
 		Quaternion rotation = Quaternion.Euler(-90, 0, 0); //rotates x to make box standing up
 		Instantiate(boxList[i], transform.position, rotation);
