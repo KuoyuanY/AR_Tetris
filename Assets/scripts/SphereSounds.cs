f5cab0597ffde33374d34ec4a1a3575c7ae396eb
Kuoyuan Yang <1kuoyuanyang@gmail.com>
2017-04-16 03:02:30 -0400

    testing

---
+++ Assets/scripts/SphereSounds.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereSounds : MonoBehaviour
+{
+	AudioSource audioSource = null;
+	AudioClip impactClip = null;
+
+	void Start()
+	{
+		// Add an AudioSource component and set up some defaults
+		audioSource = gameObject.AddComponent<AudioSource>();
+		audioSource.playOnAwake = false;
+		audioSource.spatialize = true;
+		audioSource.spatialBlend = 1.0f;
+		audioSource.dopplerLevel = 0.0f;
+		audioSource.rolloffMode = AudioRolloffMode.Logarithmic;
+		audioSource.maxDistance = 20f;
+
+		// Load the Sphere sounds from the Resources folder
+		impactClip = Resources.Load<AudioClip>("Impact");
+	}
+
+	// Occurs when this object starts colliding with another object
+	void OnCollisionEnter(Collision collision)
+	{
+		// Play an impact sound if the sphere impacts strongly enough.
+		if (collision.relativeVelocity.magnitude >= 0.1f)
+		{
+			audioSource.clip = impactClip;
+			audioSource.Play();
+		}
+	}
+		
+}
--- Assets/scripts/spawn.cs
+++ Assets/scripts/spawn.cs
@@ -15,7 +15,9 @@ public class spawn : MonoBehaviour {
 		++score;
 		int i = Random.Range(0, boxList.Length);
 		Quaternion rotation = Quaternion.Euler(-90, 0, 0); //rotates x to make box standing up
-		Instantiate(boxList[i], transform.position, rotation);
+		var go = Instantiate(boxList[i], transform.position, rotation) as GameObject;
+		go.AddComponent<SphereSounds> ();
+
 	}
 	
 	// Update is called once per frame
