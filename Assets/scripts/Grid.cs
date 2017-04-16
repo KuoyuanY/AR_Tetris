9d158969c11871b8081cb056036310c70e7938d5
Kuoyuan Yang <1kuoyuanyang@gmail.com>
2017-04-15 21:44:32 -0400

    size of border...

---
+++ Assets/scripts/Grid.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+	
+	public static int w = 9;
+	public static int h = 10;
+	public static Transform[,] grid = new Transform[w, h];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public static Vector2 roundVec2(Vector2 v) {//returns a new vector with whole number position
+		return new Vector2(Mathf.Round(v.x),
+			Mathf.Round(v.y)); 
+	}
+	public static bool insideBorder(Vector2 pos) {//checks if a block is within border
+		return ((int)pos.x >= -1 &&
+			(int)pos.x < w &&
+			(int)pos.y >= -6.1);
+	}
+	public static void deleteRow(int y) {//deletes the row that's full
+		for (int x = -1; x < w; ++x) {
+			Destroy(grid[x, y].gameObject);
+			grid[x, y] = null;
+		}
+	}
+	public static void decreaseRow(int y) {//move parameter row down by 1
+		for (int x = -1; x < w; ++x) {
+			if (grid[x, y] != null) {
+				// Move one towards bottom
+				grid[x, y-1] = grid[x, y];
+				grid[x, y] = null;
+
+				// Update Block position in world
+				grid[x, y-1].position += new Vector3(0, -1, 0);
+			}
+		}
+	}
+	public static void decreaseRowsAbove(int y) {//move all rows above down by 1 
+		for (int i = y; i > -10 && i < h; ++i)
+			decreaseRow(i);
+	}
+	public static bool isRowFull(int y) {//checks if parameter row is full
+		for (int x = 0; x < w; ++x)
+			if (grid[x, y] == null)
+				return false;
+		return true;
+	}
+	public static void deleteFullRows() {//deletes rows that are full
+		for (int y = -10; y < 10; ++y) {
+			if (isRowFull(y)) {
+				deleteRow(y);
+				decreaseRowsAbove(y+1);
+				--y;
+			}
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
